c3f37a921a584024179b30d6ba9641b99c3210bc
fesilva-dev <feliper.silva011@gmail.com>
2024-04-10 20:30:07 -0300

    feat: implements persist to zustand

--- src/stores/cart.ts
+++ src/stores/cart.ts
@@ -1,5 +1,6 @@
 import { create } from 'zustand'
 import { IMovie } from '../types'
+import { createJSONStorage, persist } from 'zustand/middleware'
 
 export interface IProduct extends IMovie {
   quantity: number
@@ -12,41 +13,47 @@ interface ICartStore {
   updateProductQuantity: (id: number, quantity: number) => void
 }
 
-export const useCartStore = create<ICartStore>(
-  (set, get) => ({
-    products: [],
-    addProductToCart: (product: IMovie) => {
-      const allProducts = get().products
-      const productIndex = allProducts.findIndex(item => item.id === product.id)
-
-      if (productIndex === -1) {
-        return set(() => ({ products: [...get().products, {...product, quantity: 1} ] }))
-      }
-
-      const updatedProducts = [...allProducts]
-      updatedProducts[productIndex].quantity += 1
-      set({ products: updatedProducts })
-    },
-    removeProductFromCart: (id: number) => {
-      const allProducts = get().products
-      const remainingProducts = allProducts.filter(item => item.id !== id)
-
-      set({ products: remainingProducts })
-    },
-    updateProductQuantity: (id: number, quantity: number) => {
-      const allProducts = get().products
-      let updatedProducts = []
-
-      if (quantity === 0) {
-        updatedProducts = allProducts.filter(
-          (item) => item.id !== id
-        )
-      } else {
-        updatedProducts = allProducts.map((item) => 
-          item.id === id ? ({ ...item, quantity }) : item
-        )
-      }
-      set({ products: updatedProducts })
-    },
-  }),
+export const useCartStore = create<ICartStore>()(
+  persist(
+    (set, get) => ({
+      products: [],
+      addProductToCart: (product: IMovie) => {
+        const allProducts = get().products
+        const productIndex = allProducts.findIndex(item => item.id === product.id)
+    
+        if (productIndex === -1) {
+          return set(() => ({ products: [...get().products, {...product, quantity: 1} ] }))
+        }
+    
+        const updatedProducts = [...allProducts]
+        updatedProducts[productIndex].quantity += 1
+        set({ products: updatedProducts })
+      },
+      removeProductFromCart: (id: number) => {
+        const allProducts = get().products
+        const remainingProducts = allProducts.filter(item => item.id !== id)
+    
+        set({ products: remainingProducts })
+      },
+      updateProductQuantity: (id: number, quantity: number) => {
+        const allProducts = get().products
+        let updatedProducts = []
+    
+        if (quantity === 0) {
+          updatedProducts = allProducts.filter(
+            (item) => item.id !== id
+          )
+        } else {
+          updatedProducts = allProducts.map((item) => 
+            item.id === id ? ({ ...item, quantity }) : item
+          )
+        }
+        set({ products: updatedProducts })
+      },
+    }),
+    {
+      name: '@wefit-movies',
+      storage: createJSONStorage(() => localStorage)
+    }
+  )
 )
